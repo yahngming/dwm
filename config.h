cc8cd23865f804485856278f8b67efe2961cbea6
Yang Ming <yahngming@gmail.com>
2024-02-04 04:16:35 +0800

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=20", "Noto Emoji:pixelsize=20:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=18", "Noto Emoji:pixelsize=18:antialias=true:autohint=true" };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
