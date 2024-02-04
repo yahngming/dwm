afc09e3d944b6aa6588f4e8cbbd244464d24c6e8
Yang Ming <yahngming@gmail.com>
2024-02-04 22:34:02 +0800

    Update config.h

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const unsigned int alphas[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "➀", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "➀", "➁", "➂", "➃", "➄", "➅", "➆", "➇", "➈" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -47,9 +47,9 @@ static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen 
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "TILE◫",      tile },    /* first entry is default */
-	{ "FLOAT❏",     NULL },    /* no layout function means floating behavior */
-	{ "MONO▣",      monocle },
+	{ "◫",       tile },    /* first entry is default */
+	{ "❏",       NULL },    /* no layout function means floating behavior */
+	{ "▣",       monocle },
 };
 
 /* key definitions */
