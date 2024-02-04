2d0874c25d6041184e85dcae90dd537de7666930
Yang Ming <yahngming@gmail.com>
2024-02-04 23:52:54 +0800

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx       = 6;        /* gaps between windows */
 static const unsigned int snap        = 32;       /* snap pixel */
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 0;        /* 0 means bottom bar */
-static const char *fonts[]            = { "monospace:size=18", "symbol:size=16" };
+static const char *fonts[]            = { "monospace:size=18", "symbol:size=22" };
 static char normbgcolor[]             = "#222222";
 static char normbordercolor[]         = "#444444";
 static char normfgcolor[]             = "#bbbbbb";
