3c0b996266d8bd7bed2d420e91f4b70df4c4dd7f
Yang Ming <yahngming@gmail.com>
2024-02-04 22:06:43 +0800

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -14,10 +14,17 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
-static char *colors[][3] = {
+static const unsigned int baralpha  = 0xdd;
+static const unsigned int borderalpha = OPAQUE;
+static const char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+};
+static const unsigned int alphas[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { OPAQUE,      baralpha,    borderalpha },
+       [SchemeSel]  = { OPAQUE,      baralpha,    borderalpha },
 };
 
 /* tagging */
