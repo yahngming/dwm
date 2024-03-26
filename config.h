7e59944eeb33d776a993e368a2af8d3f95e7376e
Yang Ming <yahngming@gmail.com>
2024-03-27 02:41:51 +0800

    Update config.h

--- config.h
+++ config.h
@@ -47,7 +47,7 @@ static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen 
 
 static const Layout layouts[] = {
 	/* symbol    arrange function */
-	{ "    ",   tile },    /* first entry is default */
+	{ "  ◫  ",   tile },    /* first entry is default */
 	{ "  󰕔  ",   NULL },    /* no layout function means floating behavior */
 	{ "    ",   monocle },
 };
