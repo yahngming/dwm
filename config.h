22f15c692846b78a09b4ffa7591afa3f8136d3de
Yang Ming <yahngming@gmail.com>
2024-04-04 15:22:42 +0800

    Update config.h

--- config.h
+++ config.h
@@ -145,6 +145,7 @@ static const Button buttons[] = {
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button2,        spawntag,       {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            0,              Button4,        shiftview,      {.i = -1 } },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button2,        killclient,     {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
