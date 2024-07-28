11b4ce0d6fd26cc5e712dd1aced60d5c86432da3
Yang Ming <yahngming@gmail.com>
2024-07-28 13:53:31 +0800

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,6 @@
 
 /* constants */
 #define TERMINAL "st"
-#define BROWSER "firefox --profile ~/.config/firefox"
 
 /* appearance */
 static const unsigned int borderpx    = 1;        /* border pixel of windows */
@@ -87,7 +86,7 @@ static const Arg tagexec[] = {
 	{ .v = (const char*[]){ TERMINAL, "-e", "cmus", NULL } },
 	{ .v = (const char*[]){ "emby-theater", NULL } },
 	{ .v = (const char*[]){ "steam", NULL } },
-	{ .v = (const char*[]){ TERMINAL, "-e", "firefox --profile ~/.config/firefox", NULL } }
+	{ .v = (const char*[]){ "firefox", "--profile ~/.config/firefox", NULL } }
 };
 
 /* keybidings */
