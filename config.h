9bbed70726248c453ed9250aaaca5c01919417b9
Yang Ming <yahngming@gmail.com>
2024-12-01 01:00:33 +0800

    Update config.h

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static const unsigned int alphas[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰣇", "", "", "󰎞", "", "󰥠", "󰨜", "󱎓", "󰖟" };
+static const char *tags[] = { "󰣇", "", "", "󰎞", "", "󰥠", "󰨜", "󰖺", "󰖟" };
 
 static const Rule rules[] = {
 	/* xprop(1):
