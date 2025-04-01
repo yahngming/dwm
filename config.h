66de56cb998879168e944372ffe5e49307f29d82
Yang Ming <yahngming@gmail.com>
2025-04-02 01:55:50 +0800

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* constants */
 #define BROWSER "browser"
-#define TERMINAL "st"
+#define TERMINAL "$TERMINAL"
 #define TUI "tui"
 
 /* appearance */
@@ -76,7 +76,7 @@ static const char *monocles[] = { "➀", "➁", "➂", "➃", "➄", "➅", "➆
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *runcmd[] = { "dmenu_run", "-m", dmenumon, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "$TERMINAL", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 /* applications per tag */
 static const Arg tagexec[] = {
