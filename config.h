e2516d530d06805795d397496656abcb33ca6eb2
Yang Ming <yahngming@gmail.com>
2025-12-07 17:32:32 +0800

    Update config.h

--- config.h
+++ config.h
@@ -36,8 +36,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class       instance    title       tags mask     isfloating   monitor */
-	{  "center",   NULL,       NULL,       0,            1,           -1 },
+	/* class            instance   title   tags mask   isfloating   isfullscreen   monitor */
+	{  "center",        NULL,      NULL,   0,          1,           0,             -1 },
+	{  "fullscreen",    NULL,      NULL,   0,          0,           1,             -1 },
+	{  "screensaver",   NULL,      NULL,   0,          0,           1,             -1 },
 };
 
 /* layout(s) */
