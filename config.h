a9c8d38940c139af5083c760f01f60a812e44e5a
Yang Ming <yahngming@gmail.com>
2025-12-07 19:59:34 +0800

    Update config.h

--- config.h
+++ config.h
@@ -38,8 +38,8 @@ static const Rule rules[] = {
 	 */
 	/* class            instance   title   tags mask   isfloating   isfullscreen   monitor */
 	{  "center",        NULL,      NULL,   0,          1,           0,             -1 },
-	{  "fullscreen",    NULL,      NULL,   0,          1,           1,             -1 },
-	{  "screensaver",   NULL,      NULL,   0,          1,           1,             -1 },
+	{  "fullscreen",    NULL,      NULL,   0,          0,           1,             -1 },
+	{  "screensaver",   NULL,      NULL,   0,          0,           1,             -1 },
 };
 
 /* layout(s) */
