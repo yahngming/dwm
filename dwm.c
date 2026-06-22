2c95308a41cd519dc85cae067367baed7d0b7b99
Yang Ming <yahngming@gmail.com>
2026-06-23 02:16:53 +0800

    resize fullscreen windows to target monitor

--- dwm.c
+++ dwm.c
@@ -1521,6 +1521,8 @@ sendmon(Client *c, Monitor *m)
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
 	attach(c);
 	attachstack(c);
+	if (c->isfullscreen)
+		resizeclient(c, m->mx, m->my, m->mw, m->mh);
 	focus(NULL);
 	arrange(NULL);
 }
